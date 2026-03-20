3093555c41154a569ee0472032a66d6ced6b11bf
jaimin-bitontree <jaimin.k@bitontree.com>
2026-03-20 15:15:42 +0530

    fix cors error

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
